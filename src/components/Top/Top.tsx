5087f62900a57c8a01cd8f98a04a8384b1e5677e
tonouchi510 <tonouchi27@gmail.com>
2019-08-28 21:48:26 +0900

    Add Base of UserHome and Top (wip)

---
+++ src/components/Top/Top.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Top = () => (
+  <div className="container">
+    <h2>Lets join Jeeek!</h2>
+    <p>wip</p>
+  </div>
+)
+
+export default Top
---
+++ src/components/UserHome/UserHome.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+const UserHome = () => (
+  <div className="container">
+    <h2>UserHome page</h2>
+  </div>
+)
+
+export default UserHome
