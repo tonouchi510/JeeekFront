6bb02f72917f83c0d5599d00465bffcb557f78f1
tonouchi510 <tonouchi27@gmail.com>
2019-08-28 17:17:26 +0900

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  devtool: 'source-map',
+  // NODE_ENVにproductionを設定すると最適化される状態で、
+  // developmentに設定するとソースマップ有効でJSファイルが出力される。
+  mode: process.env.NODE_ENV || 'development',
+
+  entry: './src/App.tsx',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        // 拡張子.tsもしくは.tsxの場合
+        test: /\.tsx?$/,
+        // 静的チェックからのコンパイル
+        use: [
+          // 下から順に処理される
+          { loader: "babel-loader" },
+          { loader: "ts-loader" },
+          {
+            loader: 'tslint-loader',
+            options: {
+              typeCheck: true,
+              fix: true,
+              emitErrors: true
+            },
+          },
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  // import文で.tsや.tsxファイルを解決するため
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json']
+  },
+  devServer: {
+    contentBase: './dist',
+    watchContentBase: true,
+    historyApiFallback: true, // 開発時はroot以外のurlも直接指定したいので
+    inline: true,
+    hot: true,
+    open: true,
+    port: 5000,
+    overlay: {
+      warnings: true,
+      errors: true
+    }
+  }
+}
