17b600c73d09b93837c7dae1d556ceb5d0be5b69
Kazu1247 <kazuhiro.1247@gmail.com>
2019-09-01 07:49:08 +0900

    Add UserHomeProps

--- src/components/UserHome/UserHome.tsx
+++ src/components/UserHome/UserHome.tsx
@@ -1,6 +1,25 @@
 import * as React from 'react'
+import { Grid, Image } from 'semantic-ui-react'
 
-const UserHome = () => (
+export interface UserHomeProps {
+  UserName: string | null
+  UserIcon: string | null
+  Category: boolean
+  SubCategory: boolean
+  SubsubCategory: boolean
+  InputURL: string | null
+  LikeCount: number
+}
+
+const UserHome = ({
+  UserName = null,
+  UserIcon = null,
+  Category = false,
+  SubCategory = false,
+  SubsubCategory = false,
+  InputURL = null,
+  LikeCount = 0,
+}) => (
   <div className="container">
     <h2>UserHome page</h2>
   </div>
