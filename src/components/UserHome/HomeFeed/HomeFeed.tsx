ce0ea06ba3b3f53e04885e2d91cf371e7b7fd1b7
Kazu1247 <kazuhiro.1247@gmail.com>
2019-09-16 14:20:50 +0900

    tweak

--- src/components/UserHome/HomeFeed/HomeFeed.tsx
+++ src/components/UserHome/HomeFeed/HomeFeed.tsx
@@ -2,24 +2,24 @@ import React, { FC } from 'react'
 import PostCard from '../PostCard'
 
 const HomeFeed = (
-    <>
-        <div style={{ height: 30 }} className="label">
-            <h2
-                style={{ marginTop: 20, height: 30, textAlign: 'center', backgroundColor: '#EEEEEE' }}
-                className="ui header"
-            >
-                HOME
-            </h2>
-        </div>
-        <div
-            style={{ float: 'left', backgroundColor: '#EEEEEE', width: 550, height: 1800 }}
-            className="ui feed"
-        >
-            <div style={{ margin: 1 }} className="ui cards">
-                <div className="ui card">{PostCard}</div>
-            </div>
-        </div>
-    </>
+  <>
+    <div style={{ height: 30 }} className="label">
+      <h2
+        style={{ marginTop: 20, height: 30, textAlign: 'center', backgroundColor: '#EEEEEE' }}
+        className="ui header"
+      >
+        HOME
+      </h2>
+    </div>
+    <div
+      style={{ float: 'left', backgroundColor: '#EEEEEE', width: 550, height: 1800 }}
+      className="ui feed"
+    >
+      <div style={{ margin: 1 }} className="ui cards">
+        <div className="ui card">{PostCard}</div>
+      </div>
+    </div>
+  </>
 )
 
 export default HomeFeed
