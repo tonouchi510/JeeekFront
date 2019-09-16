55318cf471387f7619bf5e81e51210db592892e2
Kazu1247 <kazuhiro.1247@gmail.com>
2019-09-16 21:21:13 +0900

    tweak

--- src/actions/post.ts
+++ src/actions/post.ts
@@ -1,5 +1,5 @@
 import { User } from 'firebase'
-import { AxiosError } from 'axios';
+import { AxiosError } from 'axios'
 
 export enum PostActionType {
   POST_START = 'POST_START',
@@ -9,7 +9,7 @@ export enum PostActionType {
 
 // 現段階の理解では、以下のアクションをするユーザがどういったパラメータを持って以下のアクションに入ってくるのかということを書く
 interface PostParams {
-    IsSignedIn: boolean
+  userId: string | null
 }
 
 // ここはAPIのレスポンス内容によって修正すべき箇所。今後変えていく。
@@ -21,39 +21,62 @@ interface PostParams {
 interface PostResult {
   type: PostActionType
   payload?: {
-      users: User[]
+    users: User[]
   }
 }
 
+export const Post = {
+  start: (params: PostParams) => ({
+    type: PostActionType.POST_START,
+    payload: {
+      params,
+    },
+  }),
+
+  succeed: (params: PostParams, result: PostResult) => ({
+    type: PostActionType.POST_SUCCEED,
+    payload: {
+      params,
+      result,
+    },
+  }),
+  fail: (params: PostParams, error: AxiosError) => ({
+    type: PostActionType.POST_FAIL,
+    payload: {
+      error,
+    },
+    error: true,
+  }),
+}
 // 投稿アクションを開始するに当たってその権限的なアクション
 // このStartをトリガーにしてSagaはアクションを遂行する
-export const PostStart = (params: PostParams) => ({
-  type: PostActionType.POST_START,
-  payload: {
-    params,
-  },
-})
+// export const PostStart = (params: PostParams) => ({
+// type: PostActionType.POST_START,
+// payload: {
+// params,
+// },
+// })
 
 // 投稿に成功した場合のデータ
 // 投稿完了時にどういったデータを保持しているのか（どういったデータを表示しているのか）をここに
-export const PostSucceed = (params: PostParams): (result: PostResult) => ({
-  type: PostActionType.POST_SUCCEED,
-  payload: {
-      params,
-      result,
-  },
-})
+// export const PostSucceed = (params: PostParams): (result: PostResult) => ({
+// type: PostActionType.POST_SUCCEED,
+// payload: {
+// params,
+// result,
+// },
+// })
 
 // ここのエラー処理が必要なのかはわからん
-export const PostFail = (params: PostParams): (error: AxiosError) => ({
-  type: PostActionType.POST_FAIL,
-  payload: {
-      error
-  },
-    error: true,
-})
+// export const PostFail = (params: PostParams): (error: AxiosError) => ({
+// type: PostActionType.POST_FAIL,
+// payload: {
+// error
+// },
+// error: true,
+// })
 
 export type PostAction =
-    | ReturnType<typeof PostStart>
-    | ReturnType<typeof PostSucceed>
-    | ReturnType<typeof PostFail>;
+  | ReturnType<typeof Post.start>
+  | ReturnType<typeof Post.succeed>
+  | ReturnType<typeof Post.fail>
