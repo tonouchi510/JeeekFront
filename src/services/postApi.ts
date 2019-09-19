fe6458b303bafc62ff31bc75d9e041371f0bb187
Kazu1247 <kazuhiro.1247@gmail.com>
2019-09-19 22:53:00 +0900

    Add servicers

--- src/services/postApi.ts
+++ src/services/postApi.ts
@@ -1,6 +1,7 @@
 // ここは、APIを飛ばして返ってきたデータをPost配列に入れておくための配列を用意している
 // モック的に一時的にUser配列にしてあるけど実際にはpostContent的なのが良さそう。
 import axios from 'axios'
+
 import { User } from './models'
 
 interface ApiConfig {
@@ -8,12 +9,14 @@ interface ApiConfig {
   timeout: number
 }
 
-// 主導投稿するAPIのURLをここに入力する
+// 手動投稿するAPIのURLをここに入力する
 const DEFAULT_API_CONFIG: ApiConfig = {
   baseURL: 'https://api.github.com',
   timeout: 7000,
 }
 
+// このへんはよくわかっていない
+// オプション的なconfigがある場合はここに書く？
 const PostFactory = (optionConfig?: ApiConfig) => {
   const config = {
     ...DEFAULT_API_CONFIG,
@@ -22,19 +25,15 @@ const PostFactory = (optionConfig?: ApiConfig) => {
 
   const instance = axios.create(config)
 
-  // userIdがあるユーザに対してそのuserIdをパラメータにして投稿APIをリクエストする
+  // userIdを持つユーザに対してそのuserIdをパラメータにして投稿APIをリクエストする
   // そしてそのレスポンスの結果を配列に入れて、その配列を返している
   const doPost = async (userId: string) => {
-    try {
-      const response = await instance.get(`/orgs/${userId}/members`)
-      if (response.status !== 200) {
-        throw new Error('Server Error')
-      }
-      const members: User[] = response.data
-      return members
-    } catch (err) {
-      throw err
+    const response = await instance.get(`/orgs/${userId}/members`)
+    if (response.status !== 200) {
+      throw new Error('Server Error')
     }
+    const members: User[] = response.data
+    return members
   }
   return doPost
 }
