54312c17fbf3df6aa22cb77f7a473ad36717388b
tonouchi510 <tonouchi27@gmail.com>
2019-09-26 00:43:03 +0900

    Add emotion

--- src/components/Top/Top.tsx
+++ src/components/Top/Top.tsx
@@ -1,8 +1,15 @@
+/** @jsx jsx */
 import React, { FC } from 'react'
+import { css, jsx } from '@emotion/core'
+
+const wrapper = css`
+  font-size: 30px;
+  color: #f00;
+`
 
 const Top: FC = () => (
   <div className="container">
-    <h2>Lets join Jeeek!</h2>
+    <h2 css={wrapper}>Lets join Jeeek!</h2>
     <p>wip</p>
   </div>
 )
