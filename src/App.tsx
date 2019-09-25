38800e1326abf22b16d0653d5862e560b53bc054
Kazu1247 <kazuhiro.1247@gmail.com>
2019-09-25 20:27:32 +0900

    Divide App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
 import React, { FC } from 'react'
-import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import firebase from 'firebase/app'
-
-import reducer from './reducers'
+import * as firebase from 'firebase/app'
 import AuthHandle from './containers/AuthHandleContainer'
+import firebaseConfig from './config/firebaseConfig'
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyB62WBiA_JWszHIRl7FrGFwK947_TwL0xo',
-  authDomain: 'jeeek-250713.firebaseapp.com',
-  databaseURL: 'https://jeeek-250713.firebaseio.com',
-  projectId: 'jeeek-250713',
-  storageBucket: 'jeeek-250713.appspot.com',
-  messagingSenderId: '440603496070',
-  appId: '1:440603496070:web:c5eb1c05ce7e278a',
-}
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 
-const store = createStore(reducer)
-
 const App: FC = () => (
-  <Provider store={store}>
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={AuthHandle} />
-      </Switch>
-    </BrowserRouter>
-  </Provider>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={AuthHandle} />
+    </Switch>
+  </BrowserRouter>
 )
 
-ReactDOM.render(<App />, document.getElementById('root'))
+export default App
