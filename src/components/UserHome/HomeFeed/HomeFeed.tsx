4322511c68312573abf6a8ec533d6c7a869bb83c
Kazu1247 <kazuhiro.1247@gmail.com>
2019-09-25 20:39:07 +0900

    Add HomeFeed component

---
+++ src/components/UserHome/HomeFeed/HomeFeed.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react'
+import PostCard from '../PostCard'
+
+const HomeFeed = (
+  <div className="ui container">
+    <div style={{ height: 30 }} className="label">
+      <h2
+        style={{
+          marginTop: 20,
+          height: 30,
+          textAlign: 'center',
+          color: '#FFFFFF',
+          backgroundColor: '#336666',
+          position: 'sticky',
+        }}
+        className="ui header"
+      >
+        HOME
+      </h2>
+    </div>
+    <div
+      style={{ float: 'left', backgroundColor: '#FFFFFF', width: 550, height: 1800 }}
+      className="ui feed"
+    >
+      <div style={{ margin: 1, width: 550, backgroundColor: '#FFFFFF' }} className="ui cards">
+        <PostCard />
+      </div>
+    </div>
+  </div>
+)
+
+export default HomeFeed
