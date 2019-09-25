3e0fd0b0226d401dffff513e738923237f2ad1b1
Kazu1247 <kazuhiro.1247@gmail.com>
2019-09-25 20:39:45 +0900

    Add TrendFeed componet

---
+++ src/components/UserHome/TrendFeed/TrendFeed.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from 'react'
+import PostCard from '../PostCard'
+
+const TrendFeed = (
+  <>
+    <div style={{ height: 30 }} className="label">
+      <h2
+        style={{
+          marginTop: 20,
+          height: 30,
+          textAlign: 'center',
+          color: '#FFFFFF',
+          backgroundColor: '#336666',
+        }}
+        className="ui header"
+      >
+        TREND
+      </h2>
+    </div>
+    <div
+      style={{ float: 'left', backgroundColor: '#FFFFFF', width: 550, height: 1800 }}
+      className="ui feed"
+    >
+      <div style={{ margin: 1, width: 550, backgroundColor: '#FFFFFF' }} className="ui cards">
+        <PostCard />
+      </div>
+    </div>
+  </>
+)
+
+export default TrendFeed
