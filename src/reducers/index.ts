377125f17f05ac345868dee34e20abf39c2dff52
Kazu1247 <kazuhiro.1247@gmail.com>
2019-09-25 20:37:29 +0900

    Add Post reducer

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,14 +1,17 @@
 import { combineReducers, Reducer } from 'redux'
 import authReducer, { AuthState } from './auth'
+import postReducer, { PostState } from './post'
 
 export type CombineReducerMap<S extends {}> = { [K in keyof S]: Reducer<S[K]> }
 
 interface CombinedState {
   auth: AuthState
+  post: PostState
 }
 
 const reducerMap: CombineReducerMap<CombinedState> = {
   auth: authReducer,
+  post: postReducer,
 }
 
 const appReducer: Reducer<CombinedState> = combineReducers<CombinedState>(reducerMap)
