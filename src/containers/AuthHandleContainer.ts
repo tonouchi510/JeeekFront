07a761bf53417bd170eecc00861b4e4517c27aec
tonouchi510 <tonouchi27@gmail.com>
2019-10-05 00:23:27 +0900

    Update authHandle container and component

--- src/containers/AuthHandleContainer.ts
+++ src/containers/AuthHandleContainer.ts
@@ -1,13 +1,31 @@
 import { connect } from 'react-redux'
+import { Dispatch } from 'redux'
+import ReduxSagaFirebase from 'redux-saga-firebase'
 import AuthHandle from '../components/AuthHandle'
 import { AuthState } from '../reducers/auth'
+import { signin, signout } from '../actions/auth'
 
 interface StateProps {
+  rsf: ReduxSagaFirebase
   isSignedIn: boolean
 }
 
-const mapStateToProps = (state: AuthState): StateProps => ({
-  isSignedIn: state.isSignedIn,
+interface DispatchProps {
+  signinStart: () => void
+  signoutStart: () => void
+}
+
+const mapStateToProps = (state: { auth: AuthState }): StateProps => ({
+  rsf: state.auth.rsf,
+  isSignedIn: state.auth.isSignedIn,
 })
 
-export default connect(mapStateToProps)(AuthHandle)
+const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => ({
+  signinStart: () => dispatch(signin.start()),
+  signoutStart: () => dispatch(signout.start()),
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(AuthHandle)
