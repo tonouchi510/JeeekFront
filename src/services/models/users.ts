5c30d98a8d01fb25b8c6016e5407758a0d294ab6
tonouchi510 <tonouchi27@gmail.com>
2019-10-05 01:37:16 +0900

    tweak

--- src/services/models/users.ts
+++ src/services/models/users.ts
@@ -1,26 +1,26 @@
 export type User = {
-    uid: string
-    email: string
-    emailVerified: boolean
-    phoneNumber?: string
-    password: string
-    displayName: string
-    photoURL?: string
-    disabled: boolean
+  uid: string
+  email: string
+  emailVerified: boolean
+  phoneNumber?: string
+  password: string
+  displayName: string
+  photoURL?: string
+  disabled: boolean
 }
 
 export type UserProfile = {
-    uid?: string
-    version: number
-    postCounter: number
-    skills?: {
-        skill_id: number
-        eval: number
-    }[]
+  uid?: string
+  version: number
+  postCounter: number
+  skills?: {
+    skill_id: number
+    eval: number
+  }[]
 }
 
 export type Follows = {
-    uid?: string
-    followings: { uid: string }[]
-    followers: { uid: string }[]
+  uid?: string
+  followings: { uid: string }[]
+  followers: { uid: string }[]
 }
