2a3c9756bbad6df0c38c0c0d241b1def6f2d14b2
Kazu1247 <kazuhiro.1247@gmail.com>
2019-10-17 21:58:02 +0900

    Divide user-profile-menu

---
+++ src/components/UserProfile/UserProfile.tsx
@@ -0,0 +1,12 @@
+/** @jsx jsx */
+import React, { FC } from 'react'
+import { jsx } from '@emotion/core'
+import Menu from './Menu'
+
+const UserProfile: FC = () => (
+  <div className="three wide column">
+    <Menu />
+  </div>
+)
+
+export default UserProfile
