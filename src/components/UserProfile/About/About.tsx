dc9d221ce545aef8778068a82ecd524f907c66c3
Kazu1247 <kazuhiro.1247@gmail.com>
2019-11-04 14:36:15 +0900

    AboutComponentのレビュー後の修正

--- src/components/UserProfile/About/About.tsx
+++ src/components/UserProfile/About/About.tsx
@@ -19,92 +19,84 @@ const About: FC<AboutProps> = ({
   editStart = () => {},
 }) => (
   <div className="content">
-    <div className="content" style={{ height: '100%' }}>
-      <h2 className="ui left floated header" style={{ marginLeft: 10, marginTop: 10 }}>
-        About
-      </h2>
-      <button
-        className="mini ui button"
-        type="button"
-        onClick={() => editStart(userProfile.uid)}
-        style={{ marginLeft: 10, marginTop: 10 }}
+    <h2 className="ui left floated header" style={{ marginLeft: '10px', marginTop: '10px' }}>
+      About
+    </h2>
+    <button
+      className="mini ui button"
+      type="button"
+      onClick={() => editStart(userProfile.uid)}
+      style={{ marginLeft: '10px', marginTop: '10px' }}
+    >
+      編集
+    </button>
+    <div className="content">
+      <h3
+        className="ui dividing teal header"
+        style={{ marginLeft: '10px', marginTop: '30px' }}
+        css={headerBackgroundStyle}
       >
-        編集
-      </button>
+        学歴
+      </h3>
       <div className="content">
-        <h3
-          className="ui dividing teal header"
-          style={{ marginLeft: 10, marginTop: 30 }}
-          css={headerBackgroundStyle}
-        >
-          学歴
-        </h3>
-        <div className="content">
-          <div className="content" style={{ height: '100%' }}>
-            {isEditMode ? (
-              <textarea> </textarea>
-            ) : (
-              <div className="description" style={{ marginLeft: 30 }}>
-                {userProfile.career.education.map((inst: Institution) => (
-                  <ul key={inst.subject}>
-                    <li style={{ float: 'left', width: 80 }}>{inst.period}</li>
-                    {inst.subject}
-                  </ul>
-                ))}
-              </div>
-            )}
+        {isEditMode ? (
+          <textarea> </textarea>
+        ) : (
+          <div className="description" style={{ marginLeft: '30px' }}>
+            {userProfile.career.education.map((inst: Institution) => (
+              <ul key={inst.subject}>
+                <li style={{ float: 'left', width: '10%' }}>{inst.period}</li>
+                <li style={{ listStyle: 'none' }}>{inst.subject}</li>
+              </ul>
+            ))}
           </div>
-        </div>
+        )}
       </div>
+    </div>
+    <div className="content">
+      <h3
+        className="ui dividing teal header"
+        style={{ marginLeft: '10px', marginTop: '15px' }}
+        css={headerBackgroundStyle}
+      >
+        職歴
+      </h3>
       <div className="content">
-        <h3
-          className="ui dividing teal header"
-          style={{ marginLeft: 10, marginTop: 15 }}
-          css={headerBackgroundStyle}
-        >
-          職歴
-        </h3>
-        <div className="content">
-          <div className="content" style={{ height: '100%' }}>
-            {isEditMode ? (
-              <textarea> </textarea>
-            ) : (
-              <div className="description" style={{ marginLeft: 30 }}>
-                {userProfile.career.workExperience.map((inst: Institution) => (
-                  <ul key={inst.subject}>
-                    <li style={{ float: 'left', width: 80 }}>{inst.period}</li>
-                    {inst.subject}
-                  </ul>
-                ))}
-              </div>
-            )}
+        {isEditMode ? (
+          <textarea> </textarea>
+        ) : (
+          <div className="description" style={{ marginLeft: '30px' }}>
+            {userProfile.career.workExperience.map((inst: Institution) => (
+              <ul key={inst.subject}>
+                <li style={{ float: 'left', width: '10%' }}>{inst.period}</li>
+                <li style={{ listStyle: 'none' }}>{inst.subject}</li>
+              </ul>
+            ))}
           </div>
-        </div>
+        )}
       </div>
+    </div>
+    <div className="content">
+      <h3
+        className="ui dividing teal header"
+        style={{ marginLeft: '10px', marginTop: '15px' }}
+        css={headerBackgroundStyle}
+      >
+        資格・受賞歴
+      </h3>
       <div className="content">
-        <h3
-          className="ui dividing teal header"
-          style={{ marginLeft: 10, marginTop: 15 }}
-          css={headerBackgroundStyle}
-        >
-          資格・受賞歴
-        </h3>
-        <div className="content">
-          <div className="content" style={{ height: '100%' }}>
-            {isEditMode ? (
-              <textarea> </textarea>
-            ) : (
-              <div className="description" style={{ marginLeft: 30 }}>
-                {userProfile.career.certification.map((inst: Institution) => (
-                  <ul key={inst.subject}>
-                    <li style={{ float: 'left', width: 80 }}>{inst.period}</li>
-                    {inst.subject}
-                  </ul>
-                ))}
-              </div>
-            )}
+        {isEditMode ? (
+          <textarea> </textarea>
+        ) : (
+          <div className="description" style={{ marginLeft: '30px' }}>
+            {userProfile.career.certification.map((inst: Institution) => (
+              <ul key={inst.subject}>
+                <li style={{ float: 'left', width: '10%' }}>{inst.period}</li>
+                <li style={{ listStyle: 'none' }}>{inst.subject}</li>
+              </ul>
+            ))}
           </div>
-        </div>
+        )}
       </div>
     </div>
   </div>
