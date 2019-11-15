c9e7fca697bb705663a21364aa4d1f21ebffde17
tonouchi510 <tonouchi27@gmail.com>
2019-11-15 14:46:05 +0900

    Update Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,26 +3,30 @@ import { RouteComponentProps, Link, withRouter } from 'react-router-dom'
 
 type NavbarProps = {} & RouteComponentProps<{}>
 
-const routeMap = ['/', '/timeline', '/profile', '/friends']
+const routeMap = ['/', '/timeline', '/profile', '/users', '/services', '/issue-board']
 const tabMap = {
   '/': 'Home',
   '/timeline': 'Timeline',
   '/profile': 'Profile',
-  '/friends': 'Friends',
+  '/users': 'Users',
+  '/services': 'Services',
+  '/issue-board': 'IssueBoard',
 }
 
 function iconMap(r) {
-  if (r === '/') return <i className="home icon" />
   if (r === '/timeline') return <i className="address card icon" />
   if (r === '/profile') return <i className="user icon" />
-  return <i className="users icon" />
+  if (r === '/users') return <i className="users icon" />
+  if (r === '/services') return <i className="handshake icon" />
+  if (r === '/issue-board') return <i className="clipboard list icon" />
+  return <i className="home icon" /> // homeのとき
 }
 
 const Navbar: FC<NavbarProps> = props => (
   <div className="ui secondary pointing menu">
     {routeMap.map(r =>
       props.location.pathname === r ? (
-        <Link key={r} className="item active" to={r}>
+        <Link key={r} className="ui teal item active" to={r}>
           <h3>
             {iconMap(r)}
             {tabMap[r]}
