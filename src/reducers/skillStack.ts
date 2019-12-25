e86cb9df97f9e35a76d62ac47dc72038f5b1e66d
Kazu1247 <kazuhiro.1247@gmail.com>
2019-12-25 16:09:22 +0900

    maintenance skillState

---
+++ src/reducers/skillStack.ts
@@ -0,0 +1,43 @@
+import { firestore } from 'firebase'
+import { Reducer } from 'redux'
+import { SkillAction, SkillActionType } from '../actions/skillStack'
+
+export interface SkillState {
+  skill: string
+  point: number
+  updateAt: firestore.Timestamp
+}
+
+const skillReducer: Reducer<SkillState[], SkillAction> = (
+  state: SkillState[],
+  action: SkillAction,
+): SkillState[] => {
+  switch (action.type) {
+    case SkillActionType.GET_SKILL_START: {
+      return {
+        ...state,
+      }
+    }
+    case SkillActionType.GET_SKILL_SUCCEED: {
+      return [
+        {
+          ...state,
+          skill: action.payload.result.skill,
+          point: action.payload.result.point,
+          updateAt: action.payload.result.updateAt,
+        },
+      ]
+    }
+    case SkillActionType.GET_SKILL_FAIL: {
+      // TODO: error処理
+      return state
+    }
+    default: {
+      return {
+        ...state,
+      }
+    }
+  }
+}
+
+export default skillReducer
