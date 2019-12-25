08d7b6699c8f3a149348b2f04fb9c4a416137fea
Kazu1247 <kazuhiro.1247@gmail.com>
2019-12-25 16:40:53 +0900

    maintenance UserState

---
+++ src/actions/authUser.ts
@@ -0,0 +1,53 @@
+export enum AuthActionType {
+  SIGNIN_START = 'SIGNIN_START',
+  SIGNIN_OK = 'SIGNIN_OK',
+  SIGNIN_FAIL = 'SIGNIN_FAIL',
+  SIGNOUT_START = 'SIGNOUT_START',
+  SIGNOUT_OK = 'SIGNOUT_OK',
+  SIGNOUT_FAIL = 'SIGNOUT_FAIL',
+}
+
+export interface AuthAction {
+  type: AuthActionType
+  payload?: {
+    params: AuthParams
+  }
+}
+
+interface AuthParams {
+  isSignedIn: boolean
+  uid: string
+  name: string
+  email: string
+  photoUrl: string
+  phoneNumber: string
+  emailVerified: string
+  selfIntroduction: string
+}
+
+export const signin = {
+  start: (): AuthAction => ({
+    type: AuthActionType.SIGNIN_START,
+  }),
+  ok: (params: AuthParams): AuthAction => ({
+    type: AuthActionType.SIGNIN_OK,
+    payload: {
+      params,
+    },
+  }),
+  fail: (): AuthAction => ({
+    type: AuthActionType.SIGNIN_FAIL,
+  }),
+}
+
+export const signout = {
+  start: (): AuthAction => ({
+    type: AuthActionType.SIGNOUT_START,
+  }),
+  ok: (): AuthAction => ({
+    type: AuthActionType.SIGNOUT_OK,
+  }),
+  fail: (): AuthAction => ({
+    type: AuthActionType.SIGNOUT_FAIL,
+  }),
+}
---
+++ src/reducers/authUser.ts
@@ -0,0 +1,79 @@
+import { Reducer } from 'redux'
+import { AuthAction, AuthActionType } from '../actions/authUser'
+
+export interface UserState {
+  isSignedIn: boolean
+  uid: string
+  name: string
+  email: string
+  photoUrl: string
+  phoneNumber: string
+  emailVerified: string
+  selfIntroduction: string
+}
+
+const authReducer: Reducer<UserState, AuthAction> = (
+  state: UserState,
+  action: AuthAction,
+): UserState => {
+  switch (action.type) {
+    case AuthActionType.SIGNIN_START: {
+      return {
+        ...state,
+      }
+    }
+    case AuthActionType.SIGNIN_OK: {
+      return {
+        ...state,
+        isSignedIn: true,
+        uid: action.payload.params.uid,
+        name: action.payload.params.name,
+        email: action.payload.params.email,
+        photoUrl: action.payload.params.photoUrl,
+        phoneNumber: action.payload.params.phoneNumber,
+        emailVerified: action.payload.params.emailVerified,
+        selfIntroduction: action.payload.params.selfIntroduction,
+      }
+    }
+
+    case AuthActionType.SIGNIN_FAIL: {
+      return {
+        ...state,
+      }
+    }
+
+    case AuthActionType.SIGNOUT_START: {
+      return {
+        ...state,
+      }
+    }
+
+    case AuthActionType.SIGNOUT_OK: {
+      return {
+        ...state,
+        isSignedIn: false,
+        uid: null,
+        name: null,
+        email: null,
+        photoUrl: null,
+        phoneNumber: null,
+        emailVerified: null,
+        selfIntroduction: null,
+      }
+    }
+
+    case AuthActionType.SIGNOUT_FAIL: {
+      return {
+        ...state,
+      }
+    }
+
+    default: {
+      return {
+        ...state,
+      }
+    }
+  }
+}
+
+export default authReducer
