97d193649ada3accd44803957caa39d81aecd7dc
Kazu1247 <kazuhiro.1247@gmail.com>
2020-01-03 19:53:22 +0900

    delete post

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,6 +1,5 @@
 import { combineReducers, Reducer } from 'redux'
 import authReducer, { AuthState } from './auth'
-import postReducer, { PostState } from './post'
 import followReducer, { FollowsState } from './follows'
 import profileReducer, { ProfileState } from './profile'
 import feedReducer, { FeedsState } from './feed'
@@ -10,7 +9,6 @@ export type CombineReducerMap<S extends {}> = { [K in keyof S]: Reducer<S[K]> }
 
 export const initialState: CombinedState = {
   auth: null,
-  post: null,
   follow: {
     followings: [],
     followers: [],
@@ -29,7 +27,6 @@ export const initialState: CombinedState = {
 
 export interface CombinedState {
   auth: AuthState
-  post: PostState
   follow: FollowsState
   profile: ProfileState
   feed: FeedsState
@@ -38,7 +35,6 @@ export interface CombinedState {
 
 const reducerMap: CombineReducerMap<CombinedState> = {
   auth: authReducer,
-  post: postReducer,
   follow: followReducer,
   profile: profileReducer,
   feed: feedReducer,
