568d42681d6422f30066ec1150fc7abe5777569f
Kazu1247 <kazuhiro.1247@gmail.com>
2020-01-10 11:48:18 +0900

    add usersContainer

---
+++ src/containers/Users/UsersContainer.tsx
@@ -0,0 +1,43 @@
+import React, { FC, useEffect } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators, Dispatch } from 'redux'
+import Users from '../../components/Users/Users'
+import { AuthUserState } from '../../reducers/auth'
+import { FollowState } from '../../reducers/follows'
+import { getFollows } from '../../actions/follows'
+
+interface StateProps {
+  follows: FollowState
+  user: AuthUserState
+}
+
+interface DispatchProps {
+  getFollowsStart: (uid: string) => void
+}
+
+type EnhancedUserProfileProps = StateProps & DispatchProps
+
+const mapStateToProps = (state: { follows: FollowState; authUser: AuthUserState }): StateProps => ({
+  follows: state.follows,
+  user: state.authUser,
+})
+
+const mapDispatchToProps = (dispatch: Dispatch): DispatchProps =>
+  bindActionCreators(
+    {
+      getFollowsStart: (uid: string) => getFollows.start({ uid }),
+    },
+    dispatch,
+  )
+
+const UsersContainer: FC<EnhancedUserProfileProps> = ({ follows, user, getFollowsStart }) => {
+  useEffect(() => {
+    getFollowsStart(user.uid)
+  }, [])
+  return <Users follows={follows} />
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(UsersContainer)
