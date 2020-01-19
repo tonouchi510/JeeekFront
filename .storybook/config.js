b42c2682b96c9e9f4ced30d2c5b0e8a6323529c1
Kazu1247 <kazuhiro.1247@gmail.com>
2020-01-19 11:26:17 +0900

    add config and addons for storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
 import { configure, addDecorator } from '@storybook/react';
-import '../src/styles/semantic.min.css'; import '../src/index.css';
+
 // storyファイルはstoriesディレクトリ配下に置くように設定
-const req = require.context('../stories', true, /.(story|stories).tsx$/);
+const req = require.context('../stories', true, /.(story|stories).(ts|js)$/);
 function loadStories() { req.keys().forEach(req);
 }
 configure(loadStories, module);
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,10 +1,12 @@
+const path = require('path');
+
 module.exports = ({ config }) => { config.module.rules.push({
-    test: /\.(ts|tsx)$/, include: [SRC_PATH],
+    test: /\.(ts|tsx|js)$/, include: path.resolve(__dirname, '../'),
     use: [ {
         loader: require.resolve('babel-loader'), options: {
             presets: [['react-app', { flow: false, typescript: true }]], }
     },
         { loader: require.resolve('react-docgen-typescript-loader') } ]
 });
-    config.resolve.extensions.push('.ts', '.tsx'); return config;
+    config.resolve.extensions.push('.ts', '.tsx','.js'); return config;
 };
