6937f24b807fa8ebd75a6fc8f8579fa0b6e01d6a
Kazu1247 <kazuhiro.1247@gmail.com>
2020-01-20 10:02:25 +0900

    storybook環境構築

---
+++ .storybook/config.js
@@ -0,0 +1,9 @@
+import { configure, addDecorator } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import { withKnobs } from '@storybook/addon-knobs';
+
+
+const req = require.context('../src/components', true, /.(story|stories).tsx$/);
+function loadStories() { addDecorator(withInfo); addDecorator(withKnobs); req.keys().forEach(req);
+}
+configure(loadStories, module);
---
+++ .storybook/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = ({ config }) => { config.module.rules.push({
+    test: /\.(ts|tsx)$/, include: path.resolve(__dirname, '../'),
+    use: [{
+        loader: require.resolve('babel-loader'), options: {
+            presets: [['react-app', { flow: false, typescript: true }]], }
+    },
+        { loader: require.resolve('react-docgen-typescript-loader') } ]
+});
+    config.resolve.extensions.push('.ts', '.tsx'); return config;
+};
