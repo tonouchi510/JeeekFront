2916eba41a204004541497ee4e1fbda2c49ba93a
Kazu1247 <kazuhiro.1247@gmail.com>
2020-01-23 22:03:50 +0900

    add profileStories

---
+++ stories/Profile/About/About.stories.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import About from '../../../src/components/Profile/About'
+import { CareerState } from '../../../src/reducers/career'
+
+export default {
+  title: 'Profile/About',
+  component: About,
+  excludeStories: /.*Data$/,
+};
+
+export const uidData: string = 'ggg'
+
+
+export const careerData: CareerState = {
+  education: [{
+    period: "2019/3",
+    subject: "筑波大学卒",
+  }],
+  workExperience:  [{
+    period: "2019/4",
+    subject: "dely株式会社",
+  }],
+  certification:  [{
+    period: "2020/4",
+    subject: "ワロタオブザイヤー",
+  }],
+}
+
+const isEditModeData: boolean = false
+
+export const about = () => <About uid={uidData} career={careerData} isEditMode={isEditModeData} />
+
+about.story = {
+  name: 'about'
+}
+
+// editStart?: (param?: string) => void
