d5f4fc610a70170ae9b07b48367f8731a28a8304
Kazu1247 <kazuhiro.1247@gmail.com>
2020-02-11 22:14:22 +0900

    semanticuiのインポートを消した

--- src/stories/Home/Home.stories.tsx
+++ src/stories/Home/Home.stories.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Home from '../../components/Home'
 import { SkillState } from '../../reducers/skillStack'
-import 'semantic-ui-css/semantic.min.css'
 
 export const skillData: SkillState[] = [
   { tag: 'React', point: 60, updateAt: null },
